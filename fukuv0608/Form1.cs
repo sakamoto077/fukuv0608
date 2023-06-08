ff01520b7b6a252cba4cbcddc0d3a28c7c5a10bb
sakamoto <130636519+sakamoto077@users.noreply.github.com>
2023-06-08 13:25:43 +0900

    復習0608

--- fukuv0608/Form1.cs
+++ fukuv0608/Form1.cs
@@ -2,9 +2,42 @@ namespace fukuv0608
 {
     public partial class Form1 : Form
     {
+        int vx = -5;
+        int vy = -5;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            label1.Text = "ç‚ñ{Å@ïj";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += vx;
+            label1.Top += vy;
+
+            if (label1.Left < 0)
+            {
+                vx = Math.Abs(vx + (vx / 10));
+
+            }
+            if (label1.Right > ClientSize.Width) //ïù
+            {
+                vx = -Math.Abs(vx + (vx / 10));
+            }
+            if (label1.Top < 0)
+            {
+                vy = Math.Abs(vy + (vy / 10));
+
+            }
+            if (label1.Bottom > ClientSize.Height)Å@//çÇÇ≥
+            {
+                vy = -Math.Abs(vy + (vy / 10));
+            }
         }
     }
 }
